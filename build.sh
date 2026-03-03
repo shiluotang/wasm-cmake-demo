b23c0ff3c9a6b20f0bbe57ef132c91e155082352
Quangang Sheng <shengquangang2015@163.com>
2026-03-03 15:31:36 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+declare readonly CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd -P)
+
+function main() {
+    declare bindir=build
+    pushd ${CURDIR} >& /dev/null
+    if [[ ! -d ${bindir} ]]; then
+        if ! mkdir ${bindir}; then
+            exit
+        fi
+    fi
+    if [[ ! -f ${bindir}/CMakeCache.txt ]]; then
+        pushd ${bindir} >& /dev/null
+        if ! emcmake cmake ..; then
+            exit
+        fi
+        popd >& /dev/null
+    fi
+    if [[ -f ${bindir}/Makefile ]]; then
+        emmake make -C ${bindir} all test
+    fi
+    pop >& /dev/null
+}
+
+main "${@}"
